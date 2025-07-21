e759b8e1fe9cafc29880636c109d6e438fd5a437
Carlos de Leon <admin@koxland.net>
2025-07-21 14:10:52 -0600

    Update HomeProductCardsContainer.jsx

--- src/components/containers/home/HomeProductCardsContainer.jsx
+++ src/components/containers/home/HomeProductCardsContainer.jsx
@@ -21,20 +21,6 @@ function HomeProductCardsContainer({
           <div className="flex flex-col gap-4">
             <div className="animate-fade-in text-left">
               <h2 className="text-lg font-bold tracking-wider text-neutral-400 uppercase">
-                {subtitle1}
-              </h2>
-              <h2 className="mb-4 text-2xl font-bold tracking-wider text-white uppercase">
-                {title1}
-              </h2>
-            </div>
-            <FilterButtonsHomeRopa viewScope={viewScope1} />
-            <ShopifyProductCard viewScope={viewScope1} />
-          </div>
-        </RevealOnScroll>
-        <RevealOnScroll>
-          <div className="flex flex-col gap-4">
-            <div className="animate-fade-in text-left">
-              <h2 className="text-lg font-bold tracking-wider text-neutral-400 uppercase">
                 {subtitle2}
               </h2>
               <h2 className="mb-4 text-2xl font-bold tracking-wider text-white uppercase">
@@ -43,6 +29,20 @@ function HomeProductCardsContainer({
             </div>
             <FilterButtonsHomeSuplementos viewScope={viewScope2} />
             <ShopifyProductCard viewScope={viewScope2} />
+          </div>
+        </RevealOnScroll>
+        <RevealOnScroll>
+          <div className="flex flex-col gap-4">
+            <div className="animate-fade-in text-left">
+              <h2 className="text-lg font-bold tracking-wider text-neutral-400 uppercase">
+                {subtitle1}
+              </h2>
+              <h2 className="mb-4 text-2xl font-bold tracking-wider text-white uppercase">
+                {title1}
+              </h2>
+            </div>
+            <FilterButtonsHomeRopa viewScope={viewScope1} />
+            <ShopifyProductCard viewScope={viewScope1} />
           </div>
         </RevealOnScroll>
       </div>
