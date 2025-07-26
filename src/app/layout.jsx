f818aea6ad7ce3e7284e9da8889384c2e176d5d0
Carlos de Leon <admin@koxland.net>
2025-07-25 21:34:17 -0600

    Update layout.jsx

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -89,30 +89,22 @@ export default function RootLayout({ children }) {
   return (
     <html lang="es">
       <head>
-        {/* Google Tag Manager */}
-        <Script id="gtm-script" strategy="afterInteractive">
+        {/* Google Analytics (gtag.js) */}
+        <Script
+          strategy="afterInteractive"
+          src="https://www.googletagmanager.com/gtag/js?id=G-HFW3LSSHEY"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
           {`
-            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-            })(window,document,'script','dataLayer','GTM-TVFJBCR8');
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-HFW3LSSHEY');
           `}
         </Script>
-        {/* End Google Tag Manager */}
+        {/* Fin Google Analytics */}
       </head>
       <body>
-        {/* Google Tag Manager (noscript) */}
-        <noscript>
-          <iframe
-            src="https://www.googletagmanager.com/ns.html?id=GTM-TVFJBCR8"
-            height="0"
-            width="0"
-            style={{ display: 'none', visibility: 'hidden' }}
-          ></iframe>
-        </noscript>
-        {/* End Google Tag Manager (noscript) */}
-
         <SplashScreen />
         <OptimizedProviders>
           <MemoizedHeader />
