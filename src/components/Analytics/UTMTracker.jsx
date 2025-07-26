4447e3c03b42a62b48153aa0b47693e4ed0ec8c3
Carlos de Leon <admin@koxland.net>
2025-07-26 01:04:16 -0600

    Update UTMTracker.jsx

--- src/components/Analytics/UTMTracker.jsx
+++ src/components/Analytics/UTMTracker.jsx
@@ -5,20 +5,19 @@ import { track } from '@vercel/analytics';
 
 export default function UTMTracker() {
   useEffect(() => {
-    if (typeof window === 'undefined') return; // Evita SSR
+    if (typeof window === 'undefined') return;
 
     const params = new URLSearchParams(window.location.search);
     const discount = params.get('discount');
     const utm_source = params.get('utm_source');
 
     if (discount || utm_source) {
-      // ⚠️ Esperar un poco para asegurar que Analytics ya está cargado
       setTimeout(() => {
         track('visit', {
           discount,
           utm_source,
         });
-      }, 1000); // 1 segundo de retraso
+      }, 1000);
     }
   }, []);
 
